6d255c7dcfccc047c0db2a4fe6a3eb76e8dab3a5
lakdesh <lakshan.gkl@gmail.com>
2020-03-29 00:10:15 +0530

    AndroidFragment

---
+++ app/src/main/java/com/terracodz/fragments/MainActivity.java
@@ -0,0 +1,40 @@
+package com.terracodz.fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void changeFragment(View view){
+        Fragment fragment ;
+
+        if(view== findViewById(R.id.btn1)){
+            fragment = new FragmentOne();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frag1, fragment);
+            ft.commit();
+        }
+
+        if (view == findViewById(R.id.btn2)){
+            fragment = new FragmentTwo();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frag1, fragment);
+            ft.commit();
+        }
+
+    }
+
+}
